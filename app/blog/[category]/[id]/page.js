ed3e0f428a3b5063aeb2f8641b2c5a4e360ab65c
A1X6 <ax.ahmed.ax@gmail.com>
2024-04-07 20:33:09 +0300

    added blogs in a file and make changes for it

--- app/blog/[category]/[id]/page.js
+++ app/blog/[category]/[id]/page.js
@@ -1,48 +1,29 @@
-import axios from "axios";
+import blogs from "@/app/blogs/blogs";
 
-async function fetchData(category) {
-  try {
-    let apiUrl;
-
-    switch (category) {
-      case "wallstreetjournal":
-        apiUrl =
-          "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=b6ed45b332c846a5b7a2ce290208c729";
-        break;
-      case "techcrunch":
-        apiUrl =
-          "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=b6ed45b332c846a5b7a2ce290208c729";
-        break;
-      case "topbusiness":
-        apiUrl =
-          "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b6ed45b332c846a5b7a2ce290208c729";
-        break;
-      case "tesla":
-        apiUrl =
-          "https://newsapi.org/v2/everything?q=tesla&from=2024-03-05&sortBy=publishedAt&apiKey=b6ed45b332c846a5b7a2ce290208c729";
-        break;
-      case "apple":
-        apiUrl =
-          "https://newsapi.org/v2/everything?q=apple&from=2024-04-04&to=2024-04-04&sortBy=popularity&apiKey=b6ed45b332c846a5b7a2ce290208c729";
-        break;
-      default:
-        console.log("Invalid category");
-        return { error: "Invalid category" };
-    }
-
-    const response = await axios.get(apiUrl);
-    const data = response.data;
-    return data.articles;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return { error: "Error fetching data" };
-  }
+function formatTimestamp(timestamp) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZone: "UTC",
+  };
+  const formattedDate = new Date(timestamp).toLocaleString("en-US", options);
+  return formattedDate;
 }
 
 export default async function Home({ params }) {
   const { category, id } = params;
-  const blogs = await fetchData(category.toLowerCase());
-  const { title, content, description, urlToImage, publishedAt } = blogs[1];
+  const {
+    title,
+    content,
+    description,
+    urlToImage,
+    publishedAt,
+    lastUpdatedAt,
+  } = blogs.find((blog) => blog.id === parseInt(id));
   return (
     <div className="full-page mt-4 mb-4 max-w-6xl mx-auto px-8 py-6 bg-white shadow-lg rounded-lg overflow-hidden">
       <div className="mr-5 ml-5 mb-5 md:flex">
@@ -61,8 +42,12 @@ export default async function Home({ params }) {
       <div className="mt-4 md:mt-0 md:ml-6">
         <p className="text-gray-500 leading-relaxed mb-4">{content}</p>
         <div className="flex justify-between text-sm">
-          <p className="text-gray-600">Created: {publishedAt}</p>
-          <p className="text-gray-600">Last Updated: </p>
+          <p className="text-gray-600">
+            Created: {formatTimestamp(publishedAt)}
+          </p>
+          <p className="text-gray-600">
+            Last Updated:{formatTimestamp(lastUpdatedAt)}
+          </p>
         </div>
       </div>
     </div>
