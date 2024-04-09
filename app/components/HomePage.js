404cd8350cc26fd86a0883f227c033e6969524b6
Nourhan <nourhanmahmed1@gmail.com>
2024-04-09 16:05:37 +0200

    homepage

---
+++ app/components/HomePage.js
@@ -0,0 +1,37 @@
+
+'use client'
+import axios from 'axios'
+import SinglePost from './SinglePost'
+import { useEffect, useState } from "react";
+
+import useGetPosts from "../hooks/useGetPosts"
+function HomePage() {
+
+    const [allPosts, setAllPosts] = useState([])
+    useEffect(() => {
+        axios.get("data.json").then(({ data }) => {
+            setAllPosts(data)
+        });
+        console.log(allPosts)
+    }, [])
+
+    // const allPosts = useGetPosts()
+
+    if (!allPosts) {
+        return (
+            <p className="font-black text-black">Loadin...</p>
+        )
+    }
+    return (
+        <>
+            <div className='flex flex-wrap w-3/4 justify-center items-start '>
+                {allPosts.map((post) => (
+                    <SinglePost key={post.id} post={post} />
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default HomePage;
+
